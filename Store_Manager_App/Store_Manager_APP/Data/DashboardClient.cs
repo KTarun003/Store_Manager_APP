2d85ce6985d4358755d3928b6961b73f6d9c2cb2
KTarun003 <tarunreddykreddy@gmail.com>
2020-09-05 15:34:54 +0530

    Add project files.

---
+++ Store_Manager_App/Store_Manager_APP/Data/DashboardClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Store_Manager_APP.Data
+{
+    class DashboardClient
+    {
+        HttpClient _client;
+
+        public DashboardClient()
+        {
+            _client = new HttpClient();
+        }
+
+        // API Calls Here :
+    }
+}
---
+++ Store_Manager_App/Store_Manager_APP/Models/Bills.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+namespace Store_Manager_APP.Models
+{
+    public class Items
+    {
+        [JsonProperty("itemId")]
+        public string ItemId { get; set; }
+
+        [JsonProperty("billId")]
+        public int BillId { get; set; }
+    }
+
+    public class Bills
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("amount")]
+        public float Amount { get; set; }
+
+        [JsonProperty("billDate")]
+        public DateTime BillDate { get; set; }
+
+        [JsonProperty("items")] 
+        public List<Items> Items { get; set; }
+
+    }
+}
