a8e268e1d893192682dcc2d78affafb1989e76a2
KTarun003 <tarunreddykreddy@gmail.com>
2020-09-05 15:39:40 +0530

    Added Rest API Clients

---
+++ Store_Manager_App/Store_Manager_APP/Data/BillsClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Store_Manager_APP.Data
+{
+    public class BillsClient
+    {
+        HttpClient _client;
+
+        public BillsClient()
+        {
+            _client = new HttpClient();
+        }
+    }
+}
