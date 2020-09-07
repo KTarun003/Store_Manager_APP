896f0e8032080d82bd82f7c0adf58816480e55bd
KTarun003 <tarunreddykreddy@gmail.com>
2020-09-08 03:29:57 +0530

    Finished Bill and Item Details

--- Store_Manager_App/Store_Manager_APP/Pages/BillDetailsPage.xaml.cs
+++ Store_Manager_App/Store_Manager_APP/Pages/BillDetailsPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using Store_Manager_APP.Data;
+using Store_Manager_APP.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +17,20 @@ namespace Store_Manager_APP.Pages
         public BillDetailsPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            Bills bill = (Bills)BindingContext;
+            InventoryClient inventoryClient = new InventoryClient();
+            List<Inventory> items = new List<Inventory>();
+            foreach (var item in bill.Items)
+            {
+                Inventory content = await inventoryClient.GetItemAsync(item.ItemId);
+                items.Add(content);
+            }
+            ItemsList.ItemsSource = items;
         }
     }
 }
